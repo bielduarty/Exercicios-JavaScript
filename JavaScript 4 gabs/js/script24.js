791ea444cff4bcbfe4b4ff609da71f997382e46d
Gabriel Rocha <164268065+bielduarty@users.noreply.github.com>
2024-09-10 21:20:11 -0300

    Add files via upload

---
+++ JavaScript 4 gabs/js/script24.js
@@ -0,0 +1,6 @@
+/* só executa quando for uma verdade */
+let contadora = 0
+do {
+    console.log(contadora)
+        contadora++
+} while (contadora<10)
---
+++ JavaScript 4 gabs/js/script25.js
@@ -0,0 +1,6 @@
+for (let hora = 0; hora < 24; hora++) {
+    for (let minuto = 0; minuto < 60; minuto++)
+        for (let segundo = 0; segundo < 60; segundo++) {
+            console.log(hora + " : " + minuto +" : " + segundo)
+        }
+}
